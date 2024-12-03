c81d048b9f2007928ae64ec4bd2b5ce1322febab
kim <138281054+antony-kimanzi@users.noreply.github.com>
2024-12-03 09:32:35 +0300

    Update option1quiz1.py

--- group11/option1quiz1.py
+++ group11/option1quiz1.py
@@ -2,7 +2,7 @@ def unique_list(li):
     original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     new_list = []
     for num in original_list:
-        if num in li:
+        if num not in li:
             new_list.append(num)
 
 return new_list
